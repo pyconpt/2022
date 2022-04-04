52178bf2949a2e28ef3beff33519f30f61f7c488
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 19:03:44 +0100

    fix header for lg and sm

--- pycon_portugal_2022/static/js/project.js
+++ pycon_portugal_2022/static/js/project.js
@@ -1,15 +1,18 @@
 
 const body = document.getElementsByTagName("body")[0];
 var mediaQ = window.matchMedia("(min-width: 992px)");
-var headerSize = 95;
-let limiter = 0;
+var headerSize = 185;
+let limiter = 100;
 
 function changeHeader() {
     const header = body.getElementsByClassName("header")[0];
+    const homeHeader = body.getElementsByClassName("home-header")[0];
 
     if (mediaQ.matches) {
         const logo = header.getElementsByTagName("img")[0];
-        const homeHeader = body.getElementsByClassName("home-header")[0];
+
+        header.style.position = "relative";
+        header.style.zIndex = "0";
 
         console.log(this.scrollTop + " " + limiter);
         if (this.scrollTop > limiter) {
@@ -25,7 +28,7 @@ function changeHeader() {
         }
     } else {
         header.style.zIndex = "9999";
-        header.style.position = "absolute";
+        header.style.position = "sticky";
     }
 
 }
