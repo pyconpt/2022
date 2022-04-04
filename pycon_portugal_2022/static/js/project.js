23e6eb52171c14290bcf5c39b47998dcd2b51520
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 16:28:26 +0100

    fix home-title-block on scroll

--- pycon_portugal_2022/static/js/project.js
+++ pycon_portugal_2022/static/js/project.js
@@ -1,1 +1,6 @@
 /* Project specific Javascript goes here. */
+function changeHeader() {
+    this.scrollTop > 32 ? document.body.getElementsByClassName("header")[0].style.zIndex = "9999" : document.body.getElementsByClassName("header")[0].style.zIndex = "0";
+}
+var body = document.getElementsByTagName("body")[0];
+body.addEventListener("scroll", changeHeader);
