b6be14211c250f4dcabfe8d6c45fda274c82a7c5
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 18:28:00 +0100

    add small navbar on scroll

--- pycon_portugal_2022/static/js/project.js
+++ pycon_portugal_2022/static/js/project.js
@@ -1,6 +1,25 @@
-/* Project specific Javascript goes here. */
+
+const body = document.getElementsByTagName("body")[0];
+let limiter = 90;
+
 function changeHeader() {
-    this.scrollTop > 32 ? document.body.getElementsByClassName("header")[0].style.zIndex = "9999" : document.body.getElementsByClassName("header")[0].style.zIndex = "0";
+    const header = body.getElementsByClassName("header")[0];
+    const logo = header.getElementsByTagName("img")[0];
+    const homeHeader = body.getElementsByClassName("home-header")[0];
+
+    console.log(this.scrollTop +" "+ limiter);
+    if (this.scrollTop > limiter) {
+        header.style.zIndex = "9999";
+        logo.src = "/static/images/logo/logo_mobile.svg";
+        header.classList.add("header-lg-show");
+        homeHeader.style.paddingTop = "185px";
+    } else {
+        header.style.zIndex = "0";
+        logo.src = "/static/images/logo/logo.svg";
+        header.classList.remove("header-lg-show");
+        homeHeader.style.paddingTop = "0px";
+    }
+
 }
-var body = document.getElementsByTagName("body")[0];
+
 body.addEventListener("scroll", changeHeader);
