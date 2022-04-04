b8e28ec7a66d3602b53930847f7ca1c4de5decc5
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-04 18:54:44 +0100

    fix header for sm

--- pycon_portugal_2022/static/js/project.js
+++ pycon_portugal_2022/static/js/project.js
@@ -1,23 +1,31 @@
 
 const body = document.getElementsByTagName("body")[0];
-let limiter = 90;
+var mediaQ = window.matchMedia("(min-width: 992px)");
+var headerSize = 95;
+let limiter = 0;
 
 function changeHeader() {
     const header = body.getElementsByClassName("header")[0];
-    const logo = header.getElementsByTagName("img")[0];
-    const homeHeader = body.getElementsByClassName("home-header")[0];
 
-    console.log(this.scrollTop +" "+ limiter);
-    if (this.scrollTop > limiter) {
-        header.style.zIndex = "9999";
-        logo.src = "/static/images/logo/logo_mobile.svg";
-        header.classList.add("header-lg-show");
-        homeHeader.style.paddingTop = "185px";
+    if (mediaQ.matches) {
+        const logo = header.getElementsByTagName("img")[0];
+        const homeHeader = body.getElementsByClassName("home-header")[0];
+
+        console.log(this.scrollTop + " " + limiter);
+        if (this.scrollTop > limiter) {
+            header.style.zIndex = "9999";
+            logo.src = "/static/images/logo/logo_mobile.svg";
+            header.classList.add("header-lg-show");
+            homeHeader.style.paddingTop = headerSize + "px";
+        } else {
+            header.style.zIndex = "0";
+            logo.src = "/static/images/logo/logo.svg";
+            header.classList.remove("header-lg-show");
+            homeHeader.style.paddingTop = "0px";
+        }
     } else {
-        header.style.zIndex = "0";
-        logo.src = "/static/images/logo/logo.svg";
-        header.classList.remove("header-lg-show");
-        homeHeader.style.paddingTop = "0px";
+        header.style.zIndex = "9999";
+        header.style.position = "absolute";
     }
 
 }
