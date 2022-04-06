87e4fe8fd99d1e857467c822e3f90f26139b7728
Murilo Rosa <mrmurilo75@gmail.com>
2022-04-06 14:29:45 +0100

    update menu

--- pycon_portugal_2022/site/context_processors.py
+++ pycon_portugal_2022/site/context_processors.py
@@ -43,7 +43,7 @@ def links(request):
             },
             'TICKETS': {
                 'dropdown': 'false',
-                'HREF': '/tickets/',
+                'href': '/tickets/',
             },
             'ABOUT': {
                 'dropdown': 'true',
